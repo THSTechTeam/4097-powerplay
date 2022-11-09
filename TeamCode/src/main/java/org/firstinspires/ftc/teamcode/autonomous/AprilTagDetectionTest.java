94188bc99cef7748090ca2d825208f639e7db3bd
Brandon <pacewicbrandon@gmail.com>
2022-11-09 14:20:25 -0800

    Cleanup of imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AprilTagDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AprilTagDetectionTest.java
@@ -8,7 +8,6 @@ import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-// import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
 
