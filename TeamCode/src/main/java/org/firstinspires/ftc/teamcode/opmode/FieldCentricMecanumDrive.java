b278b67c2ecdd87ff294885362a77e1d14b258e5
Brandon <pacewicbrandon@gmail.com>
2022-11-19 12:41:06 -0800

    Cleanups

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/FieldCentricMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/FieldCentricMecanumDrive.java
@@ -22,13 +22,12 @@ public class FieldCentricMecanumDrive extends LinearOpMode {
         }
 
         // Must be called at the beginning of each while opModeIsActive() loop.
-        // NOTE: 4097 driver station assignees controller to gamepad2 by default.
         protected void update() {
             try {
                 previous.copy(gamepad);
-                gamepad.copy(gamepad2);
+                gamepad.copy(gamepad1);
             } catch (RobotCoreException e) {
-                // Swallow exception, gamepad2 should always be valid.
+                // Swallow exception, gamepad1 should always be valid.
             }
         }
     }
