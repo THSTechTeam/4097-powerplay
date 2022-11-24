4582db1055d2e82a5c9f67babf781a50b578cf69
Brandon <pacewicbrandon@gmail.com>
2022-11-23 20:54:50 -0800

    Working on better april tag detection class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AprilTagDetectionTestNew.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.autonomous.ParkingLocationAnalyzer;
+import static org.firstinspires.ftc.teamcode.autonomous.ParkingLocationAnalyzer.ParkingLocation;
+
+@TeleOp(name="April Tag Detection Test 2", group="Autonomous")
+public class AprilTagDetectionTestNew extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        ParkingLocationAnalyzer parkingLocationAnalyzer = new ParkingLocationAnalyzer(hardwareMap);
+        ParkingLocation currentTargetParkingLocation, bestParkingLocation = null;
+        boolean tagFound = false;
+
+        // vvv The following loop replaces `waitForStart()`. vvv
+        while (!isStarted() && !isStopRequested()) {
+            currentTargetParkingLocation = parkingLocationAnalyzer.getParkingLocation();
+
+            if (currentTargetParkingLocation != null) {
+                tagFound            = true;
+                bestParkingLocation = currentTargetParkingLocation;
+            }
+
+            if (tagFound && currentTargetParkingLocation != null) {
+                telemetry.addData("Tag in sight: ", bestParkingLocation);
+            } else if (tagFound) {
+                telemetry.addData("Tag out of sight; going to: ", bestParkingLocation);
+            } else {
+                telemetry.addLine("No tag in sight; never seen a tag");
+            }
+
+            telemetry.update();
+            sleep(20); // Only attempt to get an image every 20ms.
+        }
+        // ^^^ End of waitForStart() replacement. ^^^
+
+        if (tagFound) {
+            telemetry.addData("Tag snapshot: ", bestParkingLocation);
+        } else {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+        }
+
+        // Prevent the OpMode from ending.
+        while (opModeIsActive()) {
+            sleep(1000);
+        }
+    }
+}
