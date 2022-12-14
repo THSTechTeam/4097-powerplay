c3a5880c90b8ab3eb6f36b05e3094a17dced27c9
Brandon <pacewicbrandon@gmail.com>
2022-12-13 17:54:16 -0800

    Working auto parking code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoParkingNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoParkingNew.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -14,21 +13,16 @@ import static org.firstinspires.ftc.teamcode.autonomous.ParkingLocationAnalyzer.
 @Config
 @Autonomous(name="Roadrunner Autonomous Parking", group="Autonomous")
 public class AutoParkingNew extends LinearOpMode {
-    // Regardless of parking position we first need to go to the center and push the signal cone out of the way.
-    // If we are parking in the center we stop there, otherwise continue to the next point.
-    public static Vector2d CENTER_PARKING_ONE = new Vector2d(0, 40);
-    public static Vector2d CENTER_PARKING_TWO = new Vector2d(0, 30);
-
-    // Left and right points continue from the center parking position.
-    public static Vector2d LEFT_PARKING = new Vector2d(0, 0);
-    public static Vector2d RIGHT_PARKING = new Vector2d(0, 0);
+    public static double FORWARD_DISTANCE = 46; // in
+    public static double STRAFE_DISTANCE  = 23; // in
 
     private ParkingLocationAnalyzer parkingLocationAnalyzer;
     private ParkingLocation parkingLocation;
 
-    private Trajectory forwards;
-    private Trajectory left;
-    private Trajectory right;
+    private Trajectory get_off_back_wall;
+
+    private Trajectory left_parking_one;
+    private Trajectory left_parking_two;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -39,21 +33,16 @@ public class AutoParkingNew extends LinearOpMode {
         drive.setPoseEstimate(new Pose2d());
 
         // Trajectories.
-        forwards = drive.trajectoryBuilder(new Pose2d())
-            .splineToConstantHeading(CENTER_PARKING_ONE, 0)
-            .splineToConstantHeading(CENTER_PARKING_TWO, 0)
+        get_off_back_wall = drive.trajectoryBuilder(new Pose2d())
+            .forward(5)
+            .build();
+        
+        left_parking_one = drive.trajectoryBuilder(get_off_back_wall.end())
+            .strafeLeft(STRAFE_DISTANCE)
             .build();
 
-        left = drive.trajectoryBuilder(new Pose2d())
-            .splineToConstantHeading(CENTER_PARKING_ONE, 0)
-            .splineToConstantHeading(CENTER_PARKING_TWO, 0)
-            .splineToConstantHeading(LEFT_PARKING, Math.toRadians(90))
-            .build();
-
-        right = drive.trajectoryBuilder(new Pose2d())
-            .splineToConstantHeading(CENTER_PARKING_ONE, 0)
-            .splineToConstantHeading(CENTER_PARKING_TWO, 0)
-            .splineToConstantHeading(RIGHT_PARKING, Math.toRadians(-90))
+        left_parking_two = drive.trajectoryBuilder(left_parking_one.end())
+            .forward(FORWARD_DISTANCE)
             .build();
 
         // vvv The following loop replaces `waitForStart()`. vvv
@@ -69,12 +58,16 @@ public class AutoParkingNew extends LinearOpMode {
         }
         // ^^^ End of `waitForStart()` replacement. ^^^
 
-        if (parkingLocation == ParkingLocation.LEFT) {
-            drive.followTrajectory(left);
-        } else if (parkingLocation == ParkingLocation.RIGHT) {
-            drive.followTrajectory(right);
-        } else {
-            drive.followTrajectory(forwards);
+        if (parkingLocation == null) {
+            parkingLocation = ParkingLocation.CENTER;
+        }
+
+        switch (parkingLocation) {
+            case LEFT:
+            default:
+                drive.followTrajectory(get_off_back_wall);
+                drive.followTrajectory(left_parking_one);
+                drive.followTrajectory(left_parking_two);
         }
     }
 }
