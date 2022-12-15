ca4420ab25d4239eebe832ca4d9279fe68541dc3
Brandon P <pacewicbrandon@gmail.com>
2022-12-15 09:07:13 -0800

    Roadrunner auto parking (#33)
    * Untested new parking code
    
    * Syntax error fixes
    
    * Fixed a bug with contnuity, still need to tune some values
    
    * Main Merge (#30)
    
    * PID controller improvements (#26)
    
    * David's review cm's
    
    * Removed showcases
    
    * Tuning
    
    * Added meet two teleop code
    
    * Constants update
    
    * Added constants to arm control
    
    * Added more constants to arm control
    
    * Arm control improvments
    
    * Deadwheel tuning (#29)
    
    * Untested new parking code
    
    * Syntax error fixes
    
    * Fixed a bug with contnuity, still need to tune some values
    
    * Bug fixes
    
    * Added PID Follower tuning
    
    * Delete AutoParkingNew.java
    
    * Working on roadrunner auto parking
    
    * Merge main into `roadrunner-auto-parking` (#32)
    
    * PID controller improvements (#26)
    
    * David's review cm's
    
    * Removed showcases
    
    * Tuning
    
    * Added meet two teleop code
    
    * Constants update
    
    * Added constants to arm control
    
    * Added more constants to arm control
    
    * Arm control improvments
    
    * Deadwheel tuning (#29)
    
    * Untested new parking code
    
    * Syntax error fixes
    
    * Fixed a bug with contnuity, still need to tune some values
    
    * Bug fixes
    
    * Added PID Follower tuning
    
    * Delete AutoParkingNew.java
    
    * Fixes + roadrunner drive code (#31)
    
    * Working auto parking code
    
    * Roadrunner auto parking
    
    * Old parking code override

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -9,12 +10,19 @@ public class MeepMeepTesting {
     static final double TILE_METER_SIZE = 0.6;
     static final double METERS_TO_INCHES = 39.3701;
 
+    public static Vector2d CENTER_PARKING_ONE = new Vector2d(0, 40);
+    public static Vector2d CENTER_PARKING_TWO = new Vector2d(0, 30);
+
+    // Left and right points continue from the center parking position.
+    public static Vector2d LEFT_PARKING = new Vector2d(-30, 30);
+    public static Vector2d RIGHT_PARKING = new Vector2d(0, 0);
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(30, 30, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
                                 .strafeRight(22.62)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoParking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoParking.java
@@ -1,62 +1,76 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.autonomous.ParkingLocationAnalyzer;
 import static org.firstinspires.ftc.teamcode.autonomous.ParkingLocationAnalyzer.ParkingLocation;
 
+@Config
 @Autonomous(name="Autonomous Parking", group="Autonomous")
 public class AutoParking extends LinearOpMode {
-    private static final double TILE_METER_LENGTH = 0.6;
-
-    private DcMotorEx motorFrontLeft;
-    private DcMotorEx motorFrontRight;
-    private DcMotorEx motorBackLeft;
-    private DcMotorEx motorBackRight;
-    private List<DcMotorEx> mecanumMotors;
-
-    private BNO055IMU imu;
+    public static double FORWARD_DISTANCE = 46; // in
+    public static double STRAFE_DISTANCE  = 23; // in
 
     private ParkingLocationAnalyzer parkingLocationAnalyzer;
     private ParkingLocation parkingLocation;
 
+    private Trajectory get_off_back_wall;
+
+    private Trajectory left_parking_one;
+    private Trajectory left_parking_two;
+
+    private Trajectory center_parking_one;
+    private Trajectory center_parking_two;
+    private Trajectory center_parking_three;
+
+    private Trajectory right_parking_one;
+    private Trajectory right_parking_two;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        motorFrontLeft  = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
-        motorBackLeft   = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
-        motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
-        motorBackRight  = hardwareMap.get(DcMotorEx.class, "motorBackRight");
-
-        mecanumMotors = Arrays.asList(motorFrontLeft, motorBackLeft, motorFrontRight, motorBackRight);
-
-        for (DcMotorEx motor : mecanumMotors) {
-            MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
-            motorConfigurationType.setAchieveableMaxRPMFraction(1.0);
-            motor.setMotorType(motorConfigurationType);
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        }
-
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        imu.initialize(parameters);
-
         parkingLocationAnalyzer = new ParkingLocationAnalyzer(hardwareMap);
+
+        // Roadrunner initialization.
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(new Pose2d());
+
+        // Trajectories.
+        get_off_back_wall = drive.trajectoryBuilder(new Pose2d())
+            .forward(5)
+            .build();
+        
+        left_parking_one = drive.trajectoryBuilder(get_off_back_wall.end())
+            .strafeLeft(STRAFE_DISTANCE)
+            .build();
+
+        left_parking_two = drive.trajectoryBuilder(left_parking_one.end())
+            .forward(FORWARD_DISTANCE)
+            .build();
+
+        center_parking_one = drive.trajectoryBuilder(get_off_back_wall.end())
+            .strafeLeft(STRAFE_DISTANCE)
+            .build();
+
+        center_parking_two = drive.trajectoryBuilder(center_parking_one.end())
+            .forward(FORWARD_DISTANCE)
+            .build();
+
+        center_parking_three = drive.trajectoryBuilder(center_parking_two.end())
+            .strafeRight(STRAFE_DISTANCE)
+            .build();
+
+        right_parking_one = drive.trajectoryBuilder(get_off_back_wall.end())
+            .strafeRight(STRAFE_DISTANCE)
+            .build();
+
+        right_parking_two = drive.trajectoryBuilder(right_parking_one.end())
+            .forward(FORWARD_DISTANCE)
+            .build();
 
         // vvv The following loop replaces `waitForStart()`. vvv
         while (!isStarted() && !isStopRequested()) {
@@ -71,110 +85,27 @@ public class AutoParking extends LinearOpMode {
         }
         // ^^^ End of `waitForStart()` replacement. ^^^
 
-        if (parkingLocation == ParkingLocation.LEFT) {
-            driveForwardOneTile();
-            resetHeading();
-            strafeLeftOneTile();
-            resetHeading();
-        } else if (parkingLocation == ParkingLocation.CENTER || parkingLocation == null) {
-            driveForwardOneTile();
-            resetHeading();
-        } else if (parkingLocation == ParkingLocation.RIGHT) {
-            driveForwardOneTile();
-            resetHeading();
-            strafeRightOneTile();
-            resetHeading();
-        }
-    }
-
-    private void resetHeading() {
-        int sleepBetweenStepTime = 250;
-        double headingCorrectionPower = 0.05;
-        double botHeading = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
-
-        sleep(sleepBetweenStepTime);
-
-        while (opModeIsActive() && botHeading != 0) {
-            botHeading = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
-
-            if (botHeading > 0) {
-                motorFrontLeft.setPower(headingCorrectionPower);
-                motorBackLeft.setPower(headingCorrectionPower);
-                motorFrontRight.setPower(-headingCorrectionPower);
-                motorBackRight.setPower(-headingCorrectionPower);
-            } else if (botHeading < 0) {
-                motorFrontLeft.setPower(-headingCorrectionPower);
-                motorBackLeft.setPower(-headingCorrectionPower);
-                motorFrontRight.setPower(headingCorrectionPower);
-                motorBackRight.setPower(headingCorrectionPower);
-            }
+        if (parkingLocation == null) {
+            parkingLocation = ParkingLocation.CENTER;
         }
 
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(0);
-        }
-
-        sleep(sleepBetweenStepTime);
-    }
-
-    private void driveForwardOneTile() {
-        // NOTE: Currently drives without the encoder.
-        // Configured for Rev Robotics HD Hex Motor 20:1
-        final double drivePower = 0.2;
-
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(drivePower);
-        }
-
-        sleep((long)(TILE_METER_LENGTH / drivePower * 1100));
-
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(0);
-        }
-
-        // Drive backwards slightly to clear the cone that we just pushed out of the way.
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(-drivePower);
-        }
-
-        sleep((long)(TILE_METER_LENGTH / drivePower * 250));
-
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(0);
-        }
-    }
-
-    private void strafeLeftOneTile() {
-        // NOTE: Currently drives without the encoder.
-        // Configured for Rev Robotics HD Hex Motor 20:1
-        final double drivePower = 0.15;
-
-        motorFrontLeft.setPower(-drivePower);
-        motorBackLeft.setPower(drivePower);
-        motorFrontRight.setPower(drivePower);
-        motorBackRight.setPower(-drivePower);
-
-        sleep((long)(TILE_METER_LENGTH / drivePower * 1000));
-
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(0);
-        }
-    }
-
-    private void strafeRightOneTile() {
-        // NOTE: Currently drives without the encoder.
-        // Configured for Rev Robotics HD Hex Motor 20:1
-        final double drivePower = 0.15;
-
-        motorFrontLeft.setPower(drivePower);
-        motorBackLeft.setPower(-drivePower);
-        motorFrontRight.setPower(-drivePower);
-        motorBackRight.setPower(drivePower);
-
-        sleep((long)(TILE_METER_LENGTH / drivePower * 1000));
-
-        for (DcMotorEx motor : mecanumMotors) {
-            motor.setPower(0);
+        switch (parkingLocation) {
+            case LEFT:
+                drive.followTrajectory(get_off_back_wall);
+                drive.followTrajectory(left_parking_one);
+                drive.followTrajectory(left_parking_two);
+                break;
+            case CENTER:
+                drive.followTrajectory(get_off_back_wall);
+                drive.followTrajectory(center_parking_one);
+                drive.followTrajectory(center_parking_two);
+                drive.followTrajectory(center_parking_three);
+                break;
+            case RIGHT:
+                drive.followTrajectory(get_off_back_wall);
+                drive.followTrajectory(right_parking_one);
+                drive.followTrajectory(right_parking_two);
+                break;
         }
     }
 }
