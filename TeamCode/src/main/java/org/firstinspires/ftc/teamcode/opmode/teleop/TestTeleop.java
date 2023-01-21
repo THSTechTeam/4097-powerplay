4c8131da8f3c94520b8df7083a7db14a7be5c9b7
Brandon <pacewicbrandon@gmail.com>
2023-01-21 12:02:46 -0800

    Status save

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TestTeleop.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp(name="Test TeleOp", group="TeleOp")
+public class TestTeleop extends OpMode {
+    
+}
